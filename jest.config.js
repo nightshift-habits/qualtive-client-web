dc3856af432d097c5f857d719e5475a51ea5776f
Gustaf Rosenblad <gustaf@ccode.se>
2020-08-23 12:55:14 +0200

    fix: Excluding dist and coverage from test patterns

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   clearMocks: true,
   collectCoverageFrom: ["src/**/*.{ts,js}"],
   moduleDirectories: ["node_modules", "src"],
-  testPathIgnorePatterns: ["/node_modules/"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/", "/coverage/"],
   transform: {
     "^.+\\.js?$": "babel-jest",
   },
