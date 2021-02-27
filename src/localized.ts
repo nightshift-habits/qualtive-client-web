607f92ea0f5e31cc02a358f61b12c925d125bf54
Gustaf Rosenblad <gustaf@ccode.se>
2021-02-27 18:33:29 +0100

    feat: Improved accessibility for built-in UI

--- src/localized.ts
+++ src/localized.ts
@@ -10,6 +10,11 @@ const strings = {
     "form.sent": "Thank you, sent!",
     "form.error": "Failed to send",
     "form.support": "For help and customer support, look here",
+    "form.score.0": "Hate",
+    "form.score.25": "Dislike",
+    "form.score.50": "Neutral",
+    "form.score.75": "Like",
+    "form.score.100": "Love",
   },
   "sv-se": {
     "form.title": "Lämna feedback",
@@ -22,6 +27,11 @@ const strings = {
     "form.sent": "Tack, skickad!",
     "form.error": "Kunde inte skicka",
     "form.support": "För hjälp och kundserviceärenden se hit",
+    "form.score.0": "Hata",
+    "form.score.25": "Ogilla",
+    "form.score.50": "Neutral",
+    "form.score.75": "Gilla",
+    "form.score.100": "Älska",
   },
 } as { [key: string]: { [key: string]: string } }
 
