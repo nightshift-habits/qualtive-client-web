40b6d3331ff8574d41ed809aee71bb377ae1c49d
Gustaf Rosenblad <gustaf@ccode.se>
2023-08-16 19:12:25 +0200

    fix: Demo error string interoplation

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -4,7 +4,7 @@ document.querySelectorAll("*[data-collection]").forEach((button) => {
   ;(button as HTMLButtonElement).onclick = (e) => {
     e.preventDefault()
     const collection = button.getAttribute("data-collection")
-    if (!collection) throw Error(`Missing collection on button: \(button)`)
+    if (!collection) throw Error(`Missing collection on button: ${button}`)
 
     qualtive.present(collection, {
       darkMode: (document.getElementById("darkmode") as HTMLSelectElement).value as "auto",
