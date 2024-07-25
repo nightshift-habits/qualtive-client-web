abcedb9231353b24632412b9a0610e4bdac986c4
Gustaf Rosenblad <gustaf@ccode.se>
2024-07-25 12:22:54 +0200

    chore: Using enquiry term in _parseCollection

--- src/collection.ts
+++ src/collection.ts
@@ -1,13 +1,13 @@
 export const _parseCollection = (collection: string): [string, string] => {
   if (typeof collection != "string")
-    throw Error("Invalid collection. First parameter to `post` must be formatted as `container-id/question-id`")
+    throw Error("Invalid collection. First parameter to `post` must be formatted as `container-id/enquiry-id-or-slug`")
 
   const components = collection.split("/")
   if (components.length != 2)
-    throw Error("Invalid collection. First parameter to `post` must be formatted as `container-id/question-id`")
+    throw Error("Invalid collection. First parameter to `post` must be formatted as `container-id/enquiry-id-or-slug`")
 
   if (!components[0]) throw Error("Invalid container id")
-  if (!components[1]) throw Error("Invalid question id")
+  if (!components[1]) throw Error("Invalid enquiry id")
 
   return components as [string, string]
 }
