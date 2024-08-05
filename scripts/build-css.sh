ab2bf75b4d39ff0723d2b779a4fe3510e289d4f7
Gustaf Rosenblad <gustaf@ccode.se>
2024-08-05 13:14:10 +0200

    feat: Updated build css script to support concurrent builds

--- scripts/build-css.sh
+++ scripts/build-css.sh
@@ -1,13 +1,14 @@
-BUILD_TEMP_FILE_NAME="./build-temp-css"
+BUILD_TEMP_FILE_NAME="./build-temp-css-${RANDOM}"
 FILES="./src/form/*.scss"
+TEMP_OUTPUT_FILE="./temp-css-${RANDOM}"
 OUTPUT_FILE="./src/form/styles.ts"
 
 # Encode output prefix
-echo "export const _styles = {" > $OUTPUT_FILE
+echo "export const _styles = {" > $TEMP_OUTPUT_FILE
 
 # Enumearte all scss-fiels
 for FILE in $FILES; do
-  
+
     # Build
     ./node_modules/.bin/sass \
         --no-source-map \
@@ -20,11 +21,14 @@ for FILE in $FILES; do
     NAME="${FILENAME%.*}"
 
     # Encode
-    echo "  $NAME: '$(cat "$BUILD_TEMP_FILE_NAME")'," >> $OUTPUT_FILE
+    echo "  $NAME: '$(cat "$BUILD_TEMP_FILE_NAME")'," >> $TEMP_OUTPUT_FILE
 done
 
 # Encode output suffix
-echo "}" >> $OUTPUT_FILE
+echo "}" >> $TEMP_OUTPUT_FILE
+
+# Move temp output to final destination
+mv -f "$TEMP_OUTPUT_FILE" "$OUTPUT_FILE"
 
 # Cleanup temp files
 rm $BUILD_TEMP_FILE_NAME || true
