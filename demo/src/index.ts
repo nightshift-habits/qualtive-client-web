e5b9f9fcf4a81a0a8fbc8ec6c9c3772406f0c05d
Gustaf Rosenblad <gustaf@ccode.se>
2024-08-05 13:18:55 +0200

    feat(demo): Added button for presenting static enquiry

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,26 +1,103 @@
 import * as qualtive from "qualtive-web"
 
+function present(collectionOrEnquiry: qualtive.Collection | qualtive.Enquiry) {
+  qualtive.present(collectionOrEnquiry, {
+    darkMode: (document.getElementById("darkmode") as HTMLSelectElement).value as "auto",
+    supportURL: (document.getElementById("supportURL") as HTMLInputElement).value,
+    locale: (document.getElementById("locale") as HTMLInputElement).value,
+    networking: (document.getElementById("networking") as HTMLSelectElement).value as "auto",
+    metadataCollection: (document.getElementById("metadataCollection") as HTMLSelectElement).value as "nonPersonal",
+    userTrackingConsent: (document.getElementById("userTrackingConsent") as HTMLSelectElement).value as "granted",
+    previewToken: (document.getElementById("previewToken") as HTMLInputElement).value || null,
+    onDismiss: (result) => {
+      if (!result) {
+        console.info("Dismissed without a posting.")
+        return
+      }
+      console.info("Dismissed with posting: ", result)
+    },
+  })
+}
+
 document.querySelectorAll("*[data-collection]").forEach((button) => {
   ;(button as HTMLButtonElement).onclick = (e) => {
     e.preventDefault()
     const collection = button.getAttribute("data-collection")
     if (!collection) throw Error(`Missing collection on button: ${button}`)
 
-    qualtive.present(collection, {
-      darkMode: (document.getElementById("darkmode") as HTMLSelectElement).value as "auto",
-      supportURL: (document.getElementById("supportURL") as HTMLInputElement).value,
-      locale: (document.getElementById("locale") as HTMLInputElement).value,
-      networking: (document.getElementById("networking") as HTMLSelectElement).value as "auto",
-      metadataCollection: (document.getElementById("metadataCollection") as HTMLSelectElement).value as "nonPersonal",
-      userTrackingConsent: (document.getElementById("userTrackingConsent") as HTMLSelectElement).value as "granted",
-      previewToken: (document.getElementById("previewToken") as HTMLInputElement).value || null,
-      onDismiss: (result) => {
-        if (!result) {
-          console.info("Dismissed without a posting.")
-          return
-        }
-        console.info("Dismissed with posting: ", result)
+    present(collection)
+  }
+})
+
+document.querySelectorAll("*[data-static-enquiry]").forEach((button) => {
+  ;(button as HTMLButtonElement).onclick = (e) => {
+    e.preventDefault()
+
+    present({
+      id: 7927746166530402,
+      slug: "how-does-the-client-web-library-work",
+      name: "How does the client web library work for you?",
+      pages: [
+        {
+          content: [
+            {
+              type: "score",
+              scoreType: "smilies5",
+              leadingText: null,
+              trailingText: null,
+            },
+            {
+              type: "text",
+              placeholder: "Type here…",
+            },
+            {
+              type: "attachments",
+            },
+            {
+              type: "select",
+              options: ["Option 1", "Option 2", "Option 3"],
+              allowsCustomInput: true,
+            },
+            {
+              type: "multiselect",
+              options: ["Option A", "Option B", "Option C"],
+            },
+          ],
+        },
+      ],
+      submittedPage: {
+        content: [
+          {
+            type: "name",
+          },
+          {
+            type: "userInput",
+          },
+          {
+            type: "image",
+            attachment: {
+              url: "https://storage.googleapis.com/qualtive-user/feedback/attachments/qualtive/6216079997140992",
+            },
+          },
+          {
+            type: "title",
+            text: "Så når du oss:",
+          },
+          {
+            type: "body",
+            text: "Vi svarar normal inom 48h. Vid brådskande ärenden är du välkommen att nå oss på telefon 123456673.",
+          },
+          {
+            type: "confirmationText",
+            text: "Tack, skickat!",
+          },
+        ],
       },
-    })
+      container: {
+        id: "qualtive",
+        logo: null,
+        isWhiteLabel: false,
+      },
+    } satisfies qualtive.Enquiry)
   }
 })
