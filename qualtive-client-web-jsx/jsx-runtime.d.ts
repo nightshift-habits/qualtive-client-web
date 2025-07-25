4202a66691d7fa40c52a3e7cc62b9a57e9fc2c06
Gustaf Rosenblad <gustaf@ccode.se>
2025-07-25 15:32:07 +0200

    fix: JSX runtime type definitions to work better with LSP

--- qualtive-client-web-jsx/jsx-runtime.d.ts
+++ qualtive-client-web-jsx/jsx-runtime.d.ts
@@ -1,11 +1,14 @@
-export type JSXNode = (() => JSXNode) | number | string | null | undefined
+export type JSXNode = (() => JSXNode) | number | string | boolean | null | undefined | Element | JSXNode[]
 export interface JSXChildren {
   children?: JSXNode | JSXNode[] | undefined
 }
 
 type E = Element
 
-type Props = { [key: string]: string | boolean | number | null | undefined } & JSXChildren
+type Props = {
+  children?: JSXNode | JSXNode[]
+  [key: string]: string | boolean | number | null | undefined | JSXNode | JSXNode[]
+}
 
 declare namespace JSX {
   interface IntrinsicElements {
