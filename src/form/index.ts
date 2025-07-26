916e6e722bee8be2c076a382c3d978b54f3b1fc3
Gustaf Rosenblad <gustaf@ccode.se>
2025-07-26 10:38:47 +0200

    fix: Expose form types

--- src/form/index.ts
+++ src/form/index.ts
@@ -1,3 +1,4 @@
 export { present, presentEnquiry } from "./present"
 export { renderEnquiry } from "./render/renderEnquiry"
 export { renderEnquirySubmitted } from "./render/renderEnquirySubmitted"
+export * from "./types"
